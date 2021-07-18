959d765b87fff197e792e5c3d8b72af98ebe618d
sri_charan <sri_charan@persistent.com>
2021-07-19 00:32:03 +0530

    Added order to index.html

--- app.js
+++ app.js
@@ -10,27 +10,61 @@ signupForm.addEventListener('submit',(e)=>{
      //sign up the user
      auth.createUserWithEmailAndPassword(email,password).then(cred=>{
       //    console.log(cred.user.email);
-          const modal=document.querySelector('#modal-signup');
-          M.Modal.getInstance(modal).close();
+      var username=createUsername(cred.user.email);
+      cred.displayName=username;
           signupForm.reset();
      });
 });
 
-//Listening status whether we're logged in or not.
 
+//Login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit',(e)=>{
+   //  e.preventDefault();
+
+     //get user info
+     const email = loginForm['login-email'].value;
+     const password = loginForm['login-password'].value;
+     //console.log(email,password);
+     auth.signInWithEmailAndPassword(email,password).then(cred=>{
+          loginForm.reset();
+     })
+});
+function createUsername(email)
+{
+     var ans="";
+     for (var i = 0; i < email.length; i++) {
+          if(email.charAt(i)=='@')
+          break;
+          ans+=(email.charAt(i));
+        }
+        console.log(ans);
+        return ans;
+}
+//logout
+const logout=document.querySelector('#logout');
+logout.addEventListener('click',(e)=>{
+     e.preventDefault();
+     auth.signOut();
+})
+
+//Listening status whether we're logged in or not.
+const loggedOutLinks=document.querySelectorAll('.loggedout');
+const loggedinLinks=document.querySelectorAll('.loggedin');
 auth.onAuthStateChanged(user=>{
      if (user)
      {
-         // console.log('user logged in:',user);
-         db.collection('guides').get().then((snapshot)=>{
-          setupGuides(snapshot.docs);
-          setupUI(user);
-     })
-     
+          console.log('user logged in:',user);
+          loggedOutLinks.forEach(item=>item.style.display='none');
+          loggedinLinks.forEach(item=>item.style.display='block');
+//console.log(user.email);
+          const username=document.querySelector('.username');
+          console.log(user.displayName);
+          username.innerHTML=user.email;
      }
      else{
-      //    console.log('user logged out:',user);
-      setupUI();
-      setupGuides([]);
+      console.log('user logged out:',user);
+      loggedOutLinks.forEach(item=>item.style.display='block');
+      loggedinLinks.forEach(item=>item.style.display='none');
      }
 })
